7b9b66523c805532ef038b472f47710f910e0dfa
antonio1000homens <github@alf1000.uk>
2025-11-19 12:47:17 +0000

    post to lambda

--- website/admin/admin-script.js
+++ website/admin/admin-script.js
@@ -291,7 +291,7 @@ async function refreshLambda() {
         const response = await fetch(lambdaUrl, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'text/plain;charset=UTF-8',
             },
             body: JSON.stringify(payload)
         });
@@ -333,7 +333,7 @@ async function hideEvent(eventUID) {
         const response = await fetch(lambdaUrl, {
             method: 'POST',
             headers: {
-                'Content-Type': 'application/json',
+                'Content-Type': 'text/plain;charset=UTF-8',
             },
             body: JSON.stringify(payload)
         });
