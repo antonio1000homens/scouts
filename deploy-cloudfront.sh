d2d1be46c555ad45ee801c8448050e2f7bacc071
antonio1000homens <github@alf1000.uk>
2026-03-12 12:31:40 +0000

    Support pre-cutover scouts cloudfront deploy

--- deploy-cloudfront.sh
+++ deploy-cloudfront.sh
@@ -8,6 +8,7 @@ STACK_NAME="${STACK_NAME:-scouts-cloudfront}"
 DOMAIN_NAME="${DOMAIN_NAME:-2ndtolworth.org.uk}"
 ORIGIN_DOMAIN_NAME="${ORIGIN_DOMAIN_NAME:-scouts-2ndtolworth-prod-553490163883.s3-website.eu-west-2.amazonaws.com}"
 TEMPLATE_FILE="${TEMPLATE_FILE:-cloudfront-stack.yaml}"
+ENABLE_ALIAS="${ENABLE_ALIAS:-false}"
 
 if [ -z "${CERTIFICATE_ARN:-}" ]; then
   echo "CERTIFICATE_ARN must be set to an ISSUED us-east-1 ACM certificate ARN."
@@ -23,6 +24,7 @@ AWS_PROFILE="${AWS_PROFILE_NAME}" aws cloudformation deploy \
   --capabilities CAPABILITY_NAMED_IAM \
   --parameter-overrides \
     DomainName="${DOMAIN_NAME}" \
+    EnableAlias="${ENABLE_ALIAS}" \
     CertificateArn="${CERTIFICATE_ARN}" \
     OriginDomainName="${ORIGIN_DOMAIN_NAME}"
 
