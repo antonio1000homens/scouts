8a32372c62417259c257c1ff4106f7779361c8c7
antonio1000homens <github@alf1000.uk>
2026-03-25 11:26:22 +0000

    permissions

--- aws/bootstrap/deploy.sh
+++ aws/bootstrap/deploy.sh
@@ -9,6 +9,7 @@ REGION="${AWS_REGION:-eu-west-2}"
 ACCOUNT_ID="${AWS_ACCOUNT_ID:-553490163883}"
 STACK_NAME="${STACK_NAME:-scouts-account-bootstrap}"
 ARTIFACT_BUCKET_NAME="${ARTIFACT_BUCKET_NAME:-aws2022-lambda-code-eu-west-2-553490163883}"
+CLOUDFORMATION_ROLE_ARN="${CLOUDFORMATION_ROLE_ARN:-}"
 BOOTSTRAP_PRINCIPAL_ARN="${BOOTSTRAP_PRINCIPAL_ARN:-}"
 SCOUTS_REPO_OWNER="${SCOUTS_REPO_OWNER:-antonio1000homens}"
 SCOUTS_REPO_NAME="${SCOUTS_REPO_NAME:-scouts}"
@@ -48,17 +49,27 @@ if [ "${CALLER_ACCOUNT}" != "${ACCOUNT_ID}" ]; then
   exit 1
 fi
 
-aws cloudformation deploy \
-  --region "${REGION}" \
-  --stack-name "${STACK_NAME}" \
-  --template-file "${TEMPLATE_FILE}" \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
-    BootstrapPrincipalArn="${BOOTSTRAP_PRINCIPAL_ARN}" \
-    ArtifactBucketName="${ARTIFACT_BUCKET_NAME}" \
-    ScoutsRepoOwner="${SCOUTS_REPO_OWNER}" \
-    ScoutsRepoName="${SCOUTS_REPO_NAME}" \
+CFN_ARGS=(
+  --region "${REGION}"
+  --stack-name "${STACK_NAME}"
+  --template-file "${TEMPLATE_FILE}"
+  --capabilities CAPABILITY_NAMED_IAM
+)
+
+if [ -n "${CLOUDFORMATION_ROLE_ARN}" ]; then
+  CFN_ARGS+=(--role-arn "${CLOUDFORMATION_ROLE_ARN}")
+fi
+
+CFN_ARGS+=(
+  --parameter-overrides
+    BootstrapPrincipalArn="${BOOTSTRAP_PRINCIPAL_ARN}"
+    ArtifactBucketName="${ARTIFACT_BUCKET_NAME}"
+    ScoutsRepoOwner="${SCOUTS_REPO_OWNER}"
+    ScoutsRepoName="${SCOUTS_REPO_NAME}"
     ScoutsBranch="${SCOUTS_BRANCH}"
+)
+
+aws cloudformation deploy "${CFN_ARGS[@]}"
 
 aws cloudformation describe-stacks \
   --region "${REGION}" \
